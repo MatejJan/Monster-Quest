250399d766cac836e796e970017820188eddee3e
Matej Jan <matej@retronator.com>
2023-01-31 16:31:21 +0100

    Fix for ability modifier equation.

--- 8. Monster Quest Generic and extension methods/Assets/Scripts/Model/AbilityScore.cs
+++ 8. Monster Quest Generic and extension methods/Assets/Scripts/Model/AbilityScore.cs
@@ -10,7 +10,7 @@ namespace MonsterQuest
     {   
         [field: SerializeField] public int score { get; set; }
         
-        public int modifier => (score - 10) / 2;
+        public int modifier => Mathf.FloorToInt((score - 10) / 2f);
 
         public static implicit operator int(AbilityScore abilityScore) => abilityScore.score;
     }
